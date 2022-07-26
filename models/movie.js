2b1423384c52d104eb9a2c997f75dd12ed79292c
Aleksandr Zinovev <uplink.std@gmail.com>
2022-07-27 00:01:29 +0300

    level-1 Установлена бибилиотека mongoose. Описаны модели пользователя и фильма.

---
+++ models/movie.js
@@ -0,0 +1,68 @@
+const mongoose = require('mongoose');
+const isURL = require('validator/es/lib/isURL');
+
+const movieSchema = new mongoose.Schema({
+  country: {
+    type: String,
+    required: true,
+  },
+  director: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  year: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (urlInput) => isURL(urlInput) === undefined,
+      message: (props) => `${props.value} is not a valid url!`,
+    },
+  },
+  trailerLink: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (urlInput) => isURL(urlInput) === undefined,
+      message: (props) => `${props.value} is not a valid url!`,
+    },
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (urlInput) => isURL(urlInput) === undefined,
+      message: (props) => `${props.value} is not a valid url!`,
+    },
+  },
+  owner: {
+    required: true,
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+  },
+  movieId: {
+    required: true,
+    type: mongoose.Schema.Types.ObjectId,
+  },
+  nameRU: {
+    type: String,
+    required: true,
+  },
+  nameEN: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('movie', movieSchema);
---
+++ models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const isEmail = require('validator/lib/isEmail');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: (emailInput) => isEmail(emailInput),
+      message: (props) => `${props.value} is not a valid email!`,
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+    select: false,
+  },
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+});
+
+module.exports = mongoose.model('user', userSchema);
