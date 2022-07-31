3c9f086787a36fb7e9d59060b54508fc4e384a06
Aleksandr Zinovev <uplink.std@gmail.com>
2022-07-31 17:24:02 +0300

    level-1 Маршрут movies зарегистрирован в приложении

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+app.use('/users', moviesRoute);
 app.use('/movies', moviesRoute);
 
 app.use((req, res, next) => handleError(new NotFoundError(`Ресурс не найден: ${req.originalUrl}`), res, next));
