ab224a22365b8bad430a29883b742adcfe09d7dd
Aleksandr Zinovev <uplink.std@gmail.com>
2022-07-31 17:22:22 +0300

    level-1 Добавлена валидация для маршрутов movie

--- routes/movies.js
+++ routes/movies.js
@@ -1,8 +1,39 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const { getMovies, createMovie } = require('../controllers/movies');
+const {
+  idValidator, urlValidator,
+} = require('../utils/joi-validation');
 
 router.get('/', getMovies);
-router.post('/', createMovie);
-router.delete('/:movieId', createMovie);
+router.post(
+  '/',
+  celebrate({
+    body: Joi.object().keys({
+      country: Joi.string().required(),
+      director: Joi.string().required(),
+      duration: Joi.number().required(),
+      year: Joi.string().required(),
+      description: Joi.string().required(),
+      image: urlValidator.required(),
+      trailerLink: urlValidator.required(),
+      thumbnail: urlValidator.required(),
+      owner: idValidator.required(),
+      movieId: idValidator.required(),
+      nameRU: Joi.string().required(),
+      nameEN: Joi.string().required(),
+    }),
+  }),
+  createMovie,
+);
+router.delete(
+  '/:movieId',
+  celebrate({
+    params: Joi.object().keys({
+      movieId: idValidator.required(),
+    }),
+  }),
+  createMovie,
+);
 
 module.exports = router;
